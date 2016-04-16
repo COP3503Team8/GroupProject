cf2e5a3812fa137d218722d6da45a87c45e27f31
reyjroliva <reyjroliva@ufl.edu>
2016-04-16 17:22:03 -0400

    Update Print.h

--- Print.h
+++ Print.h
@@ -6,17 +6,18 @@
 
 using namespace std;
 
+void printPlayerStats(User* user){
+    cout << "Player 1's Stats";
+    cout << "Health: " << user->getHealth() << "/" << user->getMaxHealth() << endl;
+    cout << "Level: " << user->getLevel() << endl;
+    cout << "Needed EXP to Level Up: " << user->getNeededEXP() << endl;
+
 void menu(User* user, int currentFloor){
     cout << "Current Floor: " << currentFloor << endl;
     printPlayerStats(user);
     printInventory(user);
     }
 
-void printPlayerStats(User* user){
-    cout << "Player 1's Stats";
-    cout << "Health: " << user.getHealth() << "/" << user.getMaxHealth() << endl;
-    cout << "Level: " << user.getLevel() << endl;
-    cout << "Needed EXP to Level Up: " << user.getNeededEXP() << endl;
 }
 
 
