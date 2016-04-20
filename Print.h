d59711780837ea74ea8a4187e5838f81dfff921a
Michael Chan-Pong <chenshiming@ufl.edu>
2016-04-20 03:07:21 -0400

    it's the small things that count

--- Print.h
+++ Print.h
@@ -30,7 +30,7 @@ void clearScreen(){
 
 void printStory(){
   //Prints intro story to screen
-  cout << "The Legend of Liu" << endl << endl << "There once was a student named Harrison who had trouble in COP3503.\nThe only way he would be able to pass the course would be to fight Dr. Liu in a one-on-one programming battle.\nHarrison decided to embark on the journey to Dr. Liu's office to confront him one and for all.\nOn his way, he encountered the ghosts of Liu's past, failed student, who were determined to make him one of them.\nGuarding each entrance to the next floor were numerous authorities such as the TAs and the advisors.\nWith packets of Smokin' Notes in hand, can Harrison succeed in taking down the almighty Liu?" << endl << endl;
+  cout << "The Legend of Liu" << endl << endl << "There once was a student named Harrison who had trouble in COP3503.\nThe only way he would be able to pass the course would be to fight Dr. Liu\nin a one-on-one programming battle.\nHarrison decided to embark on the journey to Dr. Liu's office to confront him one and for all.\nOn his way, he encountered the ghosts of Liu's past, failed student,\nwho were determined to make him one of them.\nGuarding each entrance to the next floor were numerous authorities\nsuch as the TAs and the advisors.\nWith packets of Smokin' Notes in hand, can Harrison succeed in taking down the almighty Liu?" << endl << endl;
   cout << "Enter in any key to continue." << endl;
 }
 
